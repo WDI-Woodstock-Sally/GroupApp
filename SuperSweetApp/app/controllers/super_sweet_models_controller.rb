b0ec45e8e0fa136643a5d5cd898870092e060a94
Charlie Powell <charliepowell1@gmail.com>
2015-05-07 15:15:18 -0500

    fixing merge conflict ;

--- SuperSweetApp/app/controllers/super_sweet_models_controller.rb
+++ SuperSweetApp/app/controllers/super_sweet_models_controller.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class SuperSweetModelsController < ApplicationController
   def index
     @superSweetModels = SuperSweetModel.all
@@ -7,38 +6,20 @@ class SuperSweetModelsController < ApplicationController
   def show
     @superSweetModel = SuperSweetModel.find(params[:id])
   end
-=======
-  class SuperSweetModelsController < ApplicationController
-    def index
-      #charlie
-    end
-
-    def show
-      #charlie
-    end
->>>>>>> 84b07d5ad31e0bb74a9f0336a471823e62ef151d
 
     def new
       @supersweetmodel = SuperSweetModel.new
     end
 
-<<<<<<< HEAD
     def create
       @supersweetmodel = SuperSweetModel.create(supersweetmodel_params)
       redirect_to '/supersweetmodels/#{supersweetmodels.id}'
     end
 
-    def edit
-      #nick
-    end
     def update
       #nick
     end
 
-    def destroy
-      #charlie
-    end
-=======
   def edit
     supersweetmodel = SuperSweetModel.find(params[:id]) #nick
   end
@@ -47,12 +28,9 @@ class SuperSweetModelsController < ApplicationController
   #   supersweetmodel.update(supersweetmodel_params)
   #   redirect_to "/supersweetmodel/#{supersweetmodel.id}"
   # end
-  
+
   def destroy
-<<<<<<< HEAD
     SuperSweetModel.destroy(params[:id])
-=======
-    #charlie
->>>>>>> c8ca0a920cb123c115ba7d7e7979f8827e3b6750
->>>>>>> 84b07d5ad31e0bb74a9f0336a471823e62ef151d
   end
+
+end
