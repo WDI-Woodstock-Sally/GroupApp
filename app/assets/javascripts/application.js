bf8bcc2533f01c685827f74215ac57ac59a91462
Charlie Powell <charliepowell1@gmail.com>
2015-05-12 09:56:23 -0500

    adds method to get all questions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,64 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(document).ready(function(evt){
+
+  // CREATE A REFERENCE TO FIREBASE
+  var messagesRef = new Firebase('https://triviabase.firebaseio.com/messages');
+  var questionRef = new Firebase('https://triviabase.firebaseio.com/questions')
+
+  $('#new-question').click(function(){
+    var questions = getQuestions();
+    var question = Math.floor(Math.random(questions))
+    console.log(questions)
+  })
+
+  var getQuestions = function(){
+    questionRef.on("value", function(snapshot) {
+      console.log(snapshot.val());
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  }
+
+
+  // REGISTER DOM ELEMENTS
+  var messageField = $('#messageInput');
+  var nameField = $('#nameInput');
+  var messageList = $('#example-messages');
+
+  // LISTEN FOR KEYPRESS EVENT
+  messageField.keypress(function (e) {
+    if (e.keyCode == 13) {
+      //FIELD VALUES
+      var username = nameField.val();
+      var message = messageField.val();
+
+      //SAVE DATA TO FIREBASE AND EMPTY FIELD
+      messagesRef.push({username:username, message:message});
+      messageField.val('');
+    }
+  });
+
+  // Add a callback that is triggered for each chat message.
+  messagesRef.limitToLast(10).on('child_added', function (snapshot) {
+    //GET DATA
+    var data = snapshot.val();
+    var username = data.username || "anonymous";
+    username +=  ": ";
+    var message = data.message;
+
+    //CREATE ELEMENTS MESSAGE & SANITIZE TEXT
+    var messageElement = $("<li class='chat-message'>");
+    var nameElement = $("<strong class='example-chat-username'>: </strong>")
+    nameElement.text(username);
+    messageElement.text(message).prepend(nameElement);
+
+    //ADD MESSAGE
+    messageList.append(messageElement)
+
+    //SCROLL TO BOTTOM OF MESSAGE LIST
+    messageList[0].scrollTop = messageList[0].scrollHeight;
+  });
+
+})
